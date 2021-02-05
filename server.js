e4a821a2cb3152cfe24baee40354f4274f883979
isplatform <iscode2018@gmail.com>
2021-02-05 11:09:15 -0300

    Teste

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(bodyParser.json())
 
 
 app.post('/teste', (req, res) => {
+    let connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASS,
+        database: process.env.MYSQL_DB
+    })
+    connection.connect();
+
 
     let intentName = req.body.queryResult.intent.displayName
 
@@ -18,32 +26,30 @@ app.post('/teste', (req, res) => {
         let cep = req.body.queryResult.parameters['cep']
         buscaCep(cep, { sync: false, timeout: 1000 })
             .then(endereco => {
-        let massas = req.body.queryResult.parameters['massas']
-        let recheio = req.body.queryResult.parameters['recheio']
-        let kitGrande = req.body.queryResult.parameters['kitGrande']
-        let nomeCliente = req.body.queryResult.parameters['nomeCliente']
-        let itemEndereco = endereco.logradouro+"-"+endereco.bairro+","+endereco.localidade+"-"+endereco.uf+"--"+endereco.cep
-        let quantidade = req.body.queryResult.parameters['quantidade']
-        let data = req.body.queryResult.parameters['data']
+                let massas = req.body.queryResult.parameters['massas']
+                let recheio = req.body.queryResult.parameters['recheio']
+                let kitGrande = req.body.queryResult.parameters['kitGrande']
+                let nomeCliente = req.body.queryResult.parameters['nomeCliente']
+                let itemEndereco = endereco.logradouro + "-" + endereco.bairro + "," + endereco.localidade + "-" + endereco.uf + "--" + endereco.cep
+                let quantidade = req.body.queryResult.parameters['quantidade']
+                let data = req.body.queryResult.parameters['data']
+
+                let query = 'insert into solicitations values ("' + massas + '","' + recheio + '","' + kitGrande + '","' + nomeCliente + '","' + itemEndereco + '","' + quantidade + '","' + data + '")'
+
+                connection.query(query, (error) => {
+                    if (error) throw error
+                    connection.end();
+                    res.json({ "fulfillmentText": "Primeiro Webhook" });
+                })
 
             })
-           
-        let connection = mysql.createConnection({
-            host : process.env.MYSQL_HOST,
-            user : process.env.MYSQL_USER,
-            password : process.env.MYSQL_PASS,
-            database : process.env.MYSQL_DB
-        })
-        connection.connect();
 
-        let query = 'insert into solicitations values ("'+massas+'","'+recheio+'","'+kitGrande+'","'+nomeCliente+'","'+itemEndereco+'","'+quantidade+'","'+data+'")'
 
-        connection.query(query, (error) =>{
-            if(error) throw error
-            connection.end();
-        })
 
-        res.json({ "fulfillmentText": "Primeiro Webhook" });
+
+
+
+
     }
     else if (intentName == "kit.famila") {
         res.json({ "fulfillmentText": "Primeiro Webhook 2" });
