fb77e15847b409d500148e39a407413619473476
isplatform <iscode2018@gmail.com>
2021-02-06 15:26:46 -0300

    Alteraçao no dotEnv

--- server.js
+++ server.js
@@ -32,30 +32,25 @@ app.post('/teste', (req, res) => {
 
     let intentName = req.body.queryResult.intent.displayName
 
-  
+
     if (intentName == "kit.grande") {
 
-        let cep = req.body.queryResult.parameters['cep']
-        buscaCep(cep, { sync: false, timeout: 1000 })
-            .then(endereco => {
-                let massas = req.body.queryResult.parameters['massas']
-                let recheio = req.body.queryResult.parameters['recheio']
-                let nomeItem = req.body.queryResult.parameters['nomeItem']
-                let nomeCliente = req.body.queryResult.parameters['nomeCliente']
-                let itemEndereco = endereco.logradouro + "-" + endereco.bairro + "," + endereco.localidade + "-" + endereco.uf + "--" + endereco.cep
-                let quantidade = req.body.queryResult.parameters['quantidade']
-                let data = req.body.queryResult.parameters['data']
+        let massas = req.body.queryResult.parameters['massas']
+        let recheio = req.body.queryResult.parameters['recheio']
+        let nomeItem = req.body.queryResult.parameters['nomeItem']
+        let nomeCliente = req.body.queryResult.parameters['nomeCliente']
+        let itemEndereco = req.body.queryResult.parameters['itemEndereco']
+        let quantidade = req.body.queryResult.parameters['quantidade']
+        let data = req.body.queryResult.parameters['data']
 
-                
-                connection.query("insert into solicitations values ('" + massas + "','" + recheio + "','" + nomeItem + "','" + nomeCliente + "','" + itemEndereco + "','" + quantidade + "','" + data + "')",
 
-                    function (error, results, fields) {
-                        if (error) throw error;
-                        connection.end()
-                        res.json({ "fulfillmentText": "Primeiro Webhook" });
-                    })
-            })
+        const query = 'insert into solicitations values ("'+massas+ '","'+recheio+'","'+nomeItem+'","'+nomeCliente+'","'+itemEndereco+'","'+quantidade+'","'+data+'")';
 
+        connection.query(query, function (error, results, fields) {
+            if (error) throw error;
+            connection.end();
+            response.json({"fulfillmentText" :"Contato Inserido com Sucesso" })
+         }); 
     }
     else if (intentName == "kit.famila") {
         res.json({ "fulfillmentText": "Primeiro Webhook 2" });
